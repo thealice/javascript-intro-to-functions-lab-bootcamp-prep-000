031fbc645aa3a418c328f50bd6ee31124eb82b25
Alice Freda <alicefreda@gmail.com>
2017-10-13 00:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if string === 'hello' {
-    return "I can't hear you!";
-  } else if string === 'HELLO' {
-    return "YES INDEED!";
-  } else if string === 'I love you, Grandma.' {
-    return "I love you, too.";
+  if (string === 'hello') {
+    return 'I can\'t hear you!';
+  } else if (string === 'HELLO') {
+    return 'YES INDEED!';
+  } else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
   }
 }
